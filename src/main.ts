fe7232bf46f47e2688c65e45164cca0816963d6f
Javilone <javilone.blummer@gmail.com>
2024-04-24 18:34:06 +0200

    arreglo de tipado

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,66 @@
 import "./style.css";
 
-const flechaIzq = document.getElementById("flechaIzq") as HTMLImageElement;
-const flechaDer = document.getElementById("flechaDer") as HTMLImageElement;
+const flechaIzq = document.getElementById("flechaIzq");
+const flechaDer = document.getElementById("flechaDer");
 const numeroTurno = document.getElementById(
   "numero-turno"
 ) as HTMLHeadingElement;
-const reset = document.getElementById("reset-button") as HTMLButtonElement;
-const inputButton = document.getElementById(
-  "input-button"
-) as HTMLButtonElement;
-const inputTurnos = document.getElementById("input-turnos") as HTMLInputElement;
-const textoTurnos = document.getElementById(
-  "texto-turno"
-) as HTMLParagraphElement;
+const reset = document.getElementById("reset-button");
+const inputButton = document.getElementById("input-button");
+const inputTurnos = document.getElementById("input-turnos");
+const textoTurnos = document.getElementById("texto-turno");
 
 let turno = 1;
 
-function cambiarTurno(cambiar: string) {
-  if (cambiar === "flechaDer" && turno < 99) {
-    turno++;
-    numeroTurno.innerText = turno.toString().padStart(2, "0");
-    textoTurnos.innerText = "Por favor, acérquese al mostrador";
-  } else if (cambiar === "flechaIzq" && turno > 1) {
-    turno--;
-    numeroTurno.innerText = turno.toString().padStart(2, "0");
-    textoTurnos.innerText = "Por favor, acérquese al mostrador";
-  } else if (cambiar === "reset") {
-    turno = 1;
-    numeroTurno.innerText = turno.toString().padStart(2, "0");
-    textoTurnos.innerText = "Por favor, acérquese al mostrador";
-    inputTurnos.value = "";
-  } else if (cambiar === "input") {
-    turno = parseInt(inputTurnos.value);
-    if (turno > 99 || turno < 1) {
-      numeroTurno.innerText = "¡Error!";
-      textoTurnos.innerText = "Fuera de rango: Entre 1 y 99";
-    } else {
-      numeroTurno.innerText = inputTurnos.value.padStart(2, "0");
+function cambiarTurno(cambiar: string): void {
+  if (
+    inputTurnos !== null &&
+    inputTurnos !== undefined &&
+    inputTurnos instanceof HTMLInputElement &&
+    textoTurnos instanceof HTMLParagraphElement
+  ) {
+    if (cambiar === "flechaDer" && turno < 99) {
+      turno++;
+      numeroTurno.innerText = turno.toString().padStart(2, "0");
       textoTurnos.innerText = "Por favor, acérquese al mostrador";
+    } else if (cambiar === "flechaIzq" && turno > 1) {
+      turno--;
+      numeroTurno.innerText = turno.toString().padStart(2, "0");
+      textoTurnos.innerText = "Por favor, acérquese al mostrador";
+    } else if (cambiar === "reset") {
+      turno = 1;
+      numeroTurno.innerText = turno.toString().padStart(2, "0");
+      textoTurnos.innerText = "Por favor, acérquese al mostrador";
+      inputTurnos.value = "";
+    } else if (cambiar === "input") {
+      turno = parseInt(inputTurnos.value);
+      if (turno > 99 || turno < 1) {
+        numeroTurno.innerText = "¡Error!";
+        textoTurnos.innerText = "Fuera de rango: Entre 1 y 99";
+      } else {
+        numeroTurno.innerText = inputTurnos.value.padStart(2, "0");
+        textoTurnos.innerText = "Por favor, acérquese al mostrador";
+      }
     }
   }
 }
 
-flechaDer.addEventListener("click", () => cambiarTurno("flechaDer"));
-flechaIzq.addEventListener("click", () => cambiarTurno("flechaIzq"));
-reset.addEventListener("click", () => cambiarTurno("reset"));
-inputButton.addEventListener("click", () => cambiarTurno("input"));
-inputTurnos.addEventListener("keydown", () => cambiarTurno("input"));
+if (flechaDer !== null && flechaDer !== undefined) {
+  flechaDer.addEventListener("click", () => cambiarTurno("flechaDer"));
+}
+
+if (flechaIzq !== null && flechaIzq !== undefined) {
+  flechaIzq.addEventListener("click", () => cambiarTurno("flechaIzq"));
+}
+
+if (reset !== null && reset !== undefined) {
+  reset.addEventListener("click", () => cambiarTurno("reset"));
+}
+
+if (inputTurnos !== null && inputTurnos !== undefined) {
+  inputTurnos.addEventListener("keydown", () => cambiarTurno("input"));
+}
+
+if (inputButton !== null && inputButton !== undefined) {
+  inputButton.addEventListener("click", () => cambiarTurno("input"));
+}
